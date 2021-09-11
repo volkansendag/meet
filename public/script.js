c85ba6eeefa0c7e55d64667146f9c212d11e1fd7
Volkan Şendağ <vsendag@gmail.com>
2021-09-11 17:06:35 +0300

    setup

--- public/script.js
+++ public/script.js
@@ -1,9 +1,11 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+  host: 'meet.volkansendag.com',
+  port:"443",
+  path: '/pr'
 })
+
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
